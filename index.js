69bfdab73d3934502a4871392ebd556b51db9863
thepoojari <93807424+thepoojari@users.noreply.github.com>
2021-12-08 11:06:36 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.querySelector(" #mailing-form").addEventListener("click" ,clicked);
+ 
+function clicked()
+{
+    alert("You have to Provide your Email id to login");
+}
+document.querySelector("#pass-form").addEventListener("click" ,passclick);
+function passclick()
+
+{
+    alert("You have to provide a valid password");
+}
